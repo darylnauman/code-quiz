cde014f441d8fd9e3d00e337f41589489501ae24
Daryl Nauman <darylnauman@gmail.com>
2021-05-24 19:52:52 -0400

    Setup first card to render with values; improved question option sizing

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,34 @@ var timeLeftEl = document.getElementById("time-left");
 var startButton = document.getElementById("start-button");
 var questionCardEl = document.getElementById("question-card");
 
+var questionEl = document.getElementById("question");
+var optionOneEl = document.getElementById("opt-1");
+var optionTwoEl = document.getElementById("opt-2");
+var optionThreeEl = document.getElementById("opt-3");
+var optionFourEl = document.getElementById("opt-4");
+
 var timeLeft = 5;
 
+var roundOne = {
+   question: "What does CSS stand for?",
+   optionOne: "Christmas Style Santas",
+   optionTwo: "Chrome Super Saver",
+   optionThree: "Cash Savings System",
+   optionFour: "Cascading Style Sheets",
+   correctAnswer: 4
+};
+
+function renderQuestionCard () {
+    console.log(roundOne);
+    questionEl.textContent = roundOne.question;
+    optionOneEl.textContent = roundOne.optionOne;
+    optionTwoEl.textContent = roundOne.optionTwo;
+    optionThreeEl.textContent = roundOne.optionThree;
+    optionFourEl.textContent = roundOne.optionFour;
+}
+
 function startTimer() {
+    renderQuestionCard();
     var timeInterval = setInterval(function() {
         timeLeftEl.textContent = timeLeft;
         
