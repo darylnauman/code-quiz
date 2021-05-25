d3d49f87162ff328fe7c0aa741e1c99dcf0d35ad
Daryl Nauman <darylnauman@gmail.com>
2021-05-24 22:05:57 -0400

    Initial work on displaying high scores; start button off during countdown, resets timer when pressed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ var timerEl = document.getElementById("time-countdown");
 var timeLeftEl = document.getElementById("time-left");
 var startButton = document.getElementById("start-button");
 var questionCardEl = document.getElementById("question-card");
+var viewHighScoresEl = document.getElementById("view-high-scores");
+var pageTitleEl = document.getElementById("page-title");
+var highScoresList = document.getElementById("high-scores-list");
 
 var questionEl = document.getElementById("question");
 var optionOneEl = document.getElementById("opt-1");
@@ -9,7 +12,9 @@ var optionTwoEl = document.getElementById("opt-2");
 var optionThreeEl = document.getElementById("opt-3");
 var optionFourEl = document.getElementById("opt-4");
 
-var timeLeft = 5;
+var highScores = ["Daryl", "32", "Mike", "51"];
+
+var timeLeft;
 
 var roundOne = {
    question: "What does CSS stand for?",
@@ -21,7 +26,6 @@ var roundOne = {
 };
 
 function renderQuestionCard () {
-    console.log(roundOne);
     questionEl.textContent = roundOne.question;
     optionOneEl.textContent = roundOne.optionOne;
     optionTwoEl.textContent = roundOne.optionTwo;
@@ -30,6 +34,8 @@ function renderQuestionCard () {
 }
 
 function startTimer() {
+    timeLeft = 10;
+    startButton.disabled = true;
     renderQuestionCard();
     var timeInterval = setInterval(function() {
         timeLeftEl.textContent = timeLeft;
@@ -38,11 +44,46 @@ function startTimer() {
             timeLeft--;
         } else {
             clearInterval(timeInterval);
+            startButton.disabled = false;
         }
     }, 1000);
 }
 
+function displayHighScores (event) {
+    
+    console.log(`Entered displayHighScores on a ${event.type}`);
+    console.log(`highScoresList: ${highScoresList}`);
+    // highScoresList = "";
+
+    questionCardEl.setAttribute("style", "display: none");
+
+    var storedHighScores = JSON.parse(localStorage.getItem("highScores"));
+
+    // checks if localStorage has high scores, if so assigned to local variable
+    if (storedHighScores !== null) {
+        highScores = storedHighScores;
+    }
+
+    for (var i = 0; i < highScores.length; i = i + 2 ) {
+        var name = highScores[i];
+        var score = highScores[i + 1];
+
+        console.log(`Name: ${name}, Score: ${score}`);
+
+        var li = document.createElement("li");
+        li.textContent = `Name: ${name}, Score: ${score}`;
+        console.log(li);
+
+        highScoresList.appendChild(li);
+    }
+}
+
+// Event Listeners
+
+
 startButton.addEventListener("click", startTimer);
+
+viewHighScoresEl.addEventListener("click", displayHighScores);
 
 questionCardEl.addEventListener("click", function(event) {
     var element = event.target;
@@ -52,3 +93,5 @@ questionCardEl.addEventListener("click", function(event) {
         console.log(`Option selcted: ${optionSelected}`);
     }
 });
+
+// remember to --> startButton.disabled = false;
